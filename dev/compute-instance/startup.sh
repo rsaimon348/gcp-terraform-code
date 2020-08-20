e6337c399a5ab3d5e97d80eda3ccdc51b9323be1
Raqibul Saimon <raqibul.saimonhbd@gmail.com>
2020-08-20 01:31:25 -0600

    edit startup script

--- dev/compute-instance/startup.sh
+++ dev/compute-instance/startup.sh
@@ -4,5 +4,5 @@ apt-get install apache2 -y
 apt-get install mysql-server -y
 #apt install php libapache2-mod-php -y
 echo "I am from $(hostname)" > /var/www/html/index.html
-mysql -u root -h 10.33.0.3 -p"Sa961419!" -e 'show processlist' >> /var/www/html/index.html
+mysql -u root -h 10.33.0.3 -p"[MYSQL_ROOT_PASSWORD]" -e 'show processlist' >> /var/www/html/index.html
 systemctl restart apache2
